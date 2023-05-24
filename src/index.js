034e53e4d216d4a9d2110b9f8fd9e96691016f1e
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-24 15:01:06 +0900

    base

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  WebGLRenderer,
+} from "three";
+
+const world = {};
+
+init();
+function init() {
+  const canvas = document.querySelector("#canvas");
+  const canvasRect = canvas.getBoundingClientRect();
+  console.log(canvasRect);
+  world.renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  world.renderer.setSize(canvasRect.width, canvasRect.height, false);
+  world.renderer.setPixelRatio(window.devicePixelRatio);
+  world.renderer.setClearColor(0x000000, 0.0);
+
+  world.scene = new Scene();
+
+  const cameraWidth = canvasRect.width;
+  const cameraHeight = canvasRect.height;
+  const near = 1500;
+  const far = 4000;
+  const aspect = cameraWidth / cameraHeight;
+  const cameraZ = 3000;
+  const radian = 2 * Math.atan(cameraHeight / 2 / cameraZ);
+  const fov = radian * (180 / Math.PI);
+
+  world.camera = new PerspectiveCamera(fov, aspect, near, far);
+  world.camera.position.z = cameraZ;
+
+  const geometry = new PlaneGeometry(100, 100);
+  const material = new MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new Mesh(geometry, material);
+  mesh.position.z = 0;
+  world.scene.add(mesh);
+
+  function animate() {
+    world.renderer.render(world.scene, world.camera);
+    requestAnimationFrame(animate);
+  }
+  animate();
+}

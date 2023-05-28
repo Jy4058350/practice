8191d5445fb2ae7fc701fdeaf187cb4eb23f0b31
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-28 13:37:20 +0900

    raycasterのcode exampleを貼り付ける

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -788,9 +788,63 @@ function bindResizeEvent() {
             world.camera.updateProjectionMatrix();
         }, 500);
     });
-} // Raycasterのアドレス
+}
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+function onPointerMove(event) {
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+    pointer.x = event.clientX / window.innerWidth * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+function render() {
+    // update the picking ray with the camera and pointer position
+    raycaster.setFromCamera(pointer, camera);
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects(scene.children);
+    for(let i = 0; i < intersects.length; i++)intersects[i].object.material.color.set(0xff0000);
+    renderer.render(scene, camera);
+}
+window.addEventListener("pointermove", onPointerMove);
+window.requestAnimationFrame(render); // Raycasterのアドレス
  // https://ics.media/tutorial-three/raycasting.html
  //threejs.org/docs/index.html?q=Raycaster#api/en/core/Raycaster
+ /*
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+function onPointerMove( event ) {
+
+	// calculate pointer position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+function render() {
+
+	// update the picking ray with the camera and pointer position
+	raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( scene.children );
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+
+		intersects[ i ].object.material.color.set( 0xff0000 );
+
+	}
+
+	renderer.render( scene, camera );
+
+}
+
+window.addEventListener( 'pointermove', onPointerMove );
+
+window.requestAnimationFrame(render);
+*/ 
 
 },{"three":"ktPTu","gsap":"juX9Y","smooth-scrollbar":"7azJf","gsap/ScrollTrigger":"hMdCG","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"ktPTu":[function(require,module,exports) {
 /**
